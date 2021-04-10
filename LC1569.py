8feca99558d01e37898b9c5598b5313c5c845503
jimmychancsc <54969472+jimmychancsc@users.noreply.github.com>
2021-04-10 12:08:38 -0400

    Add files via upload

---
+++ LC1569.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numOfWays(self, nums: List[int]) -> int:
+        def f(nums):
+            if len(nums) <= 2:
+                return 1
+            left = [v for v in nums if v < nums[0]]
+            right = [v for v in nums if v > nums[0]]
+
+            return comb(len(left) + len(right), len(left)) * f(left) * f(right)
+
+        return (f(nums) - 1) % (10 ** 9 + 7)
