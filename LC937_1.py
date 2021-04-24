9f97ba0c1b2ced80f5f8ecc413bcd715fc124532
jimmychancsc <54969472+jimmychancsc@users.noreply.github.com>
2021-04-23 22:14:28 -0400

    Add files via upload

---
+++ LC937_1.py
@@ -0,0 +1,16 @@
+#Reorder data in log file
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters = []
+        digits = []
+
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        letters.sort(key=lambda x: x.split()[0])
+        letters.sort(key=lambda x: x.split()[1:])
+        res = letters + digits
+        return res
