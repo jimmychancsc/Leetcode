4e2442c65017daea21df9549aee3b5ec3277a645
jimmychancsc <54969472+jimmychancsc@users.noreply.github.com>
2021-06-17 23:04:32 -0400

    Add files via upload

---
+++ LC1130.py
@@ -0,0 +1,12 @@
+def mctFromLeafValues(A):
+    res = 0
+    stack = [float('inf')]
+    for a in A:
+        while stack[-1] <= a:
+            mid = stack.pop()
+            res += mid * min(stack[-1], a)
+        stack.append(a)
+    while len(stack) > 2:
+        res += stack.pop() * stack[-1]
+    return res
+
